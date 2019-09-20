7dc34d78a08c475e93ba50347dca139e7ab3aab7
Maximiliano Fierro <elmasse@gmail.com>
2019-09-20 13:50:09 -0300

    shogi-moves package

--- packages/shogi-core/src/index.js
+++ packages/shogi-core/src/index.js
@@ -2,4 +2,5 @@
 // Copyright 2019 Wireline, Inc.
 //
 
-export * from './shogi';
+// TODO(elmasse): Export shogi-moves for now. Implement protocol, defs and state-machine.
+export * from 'shogi-moves';
